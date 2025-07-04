e54e5420504e3f4cfc8499656159f7c4154ecc0f
XIVIXMMI <lethaikhoinguyen@gmail.com>
2025-07-04 18:32:10 +0700

    fix: remove duplicate MethodArgumentNotValidException handler from GlobalExceptionHandler

--- src/main/java/com/omori/taskmanagement/springboot/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/omori/taskmanagement/springboot/exceptions/GlobalExceptionHandler.java
@@ -2,26 +2,15 @@ package com.omori.taskmanagement.springboot.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-            errors.put(error.getField(), error.getDefaultMessage())
-        );
-        return ResponseEntity.badRequest().body(errors);
-    }
 
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
