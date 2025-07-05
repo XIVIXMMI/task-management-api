2d4ea24fa18309d8bd4a82b51f0e244d47f671f6
XIVIXMMI <lethaikhoinguyen@gmail.com>
2025-07-05 22:03:37 +0700

    fix(user): regex pattern for image

--- src/main/java/com/omori/taskmanagement/springboot/validation/ValidImagePathValidator.java
+++ src/main/java/com/omori/taskmanagement/springboot/validation/ValidImagePathValidator.java
@@ -6,10 +6,11 @@ import jakarta.validation.ConstraintValidatorContext;
 public class ValidImagePathValidator implements ConstraintValidator<ValidImagePath, String> {
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        if (value == null || value.isBlank()) return false;
+        if (value == null || value.isBlank())
+            return false;
         String lower = value.toLowerCase();
         return lower.matches("^(https?://.+\\.(jpg|jpeg|png|gif))$") ||
-               lower.matches("^/.+\\.(jpg|jpeg|png|gif)$") ||
-               lower.matches("^[a-zA-Z0-9_.\\-./]+\\.(jpg|jpeg|png|gif)$");
+                lower.matches("^/[a-zA-Z0-9_.\\-/]+\\.(jpg|jpeg|png|gif)$") ||
+                lower.matches("^[a-zA-Z0-9_.\\-]+\\.(jpg|jpeg|png|gif)$");
     }
 }
